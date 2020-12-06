17662d44afdbd63acb630c382927a1e0b233becd
yanghyeonjin <guswls7081@gmail.com>
2020-12-06 13:14:13 +0900

    * prevent gone outside when item swiped

--- app/src/main/java/com/example/recyclerviewswipemenu/SwipeController.kt
+++ app/src/main/java/com/example/recyclerviewswipemenu/SwipeController.kt
@@ -1,11 +1,13 @@
 package com.example.recyclerviewswipemenu
 
+import android.graphics.Canvas
+import android.view.MotionEvent
 import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.ItemTouchHelper.LEFT
-import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.RecyclerView
 
 class SwipeController : ItemTouchHelper.Callback() {
+    private var swipeBack = false
 
     override fun getMovementFlags(
         recyclerView: RecyclerView,
@@ -24,6 +26,45 @@ class SwipeController : ItemTouchHelper.Callback() {
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
 
+    }
+
+    override fun convertToAbsoluteDirection(flags: Int, layoutDirection: Int): Int {
+        if (swipeBack) {
+            swipeBack = false
+            return 0
+        }
+        return super.convertToAbsoluteDirection(flags, layoutDirection)
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        if (actionState == ACTION_STATE_SWIPE) {
+            setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+
+    private fun setTouchListener(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        recyclerView.setOnTouchListener { view, motionEvent ->
+            swipeBack = motionEvent.action == MotionEvent.ACTION_CANCEL || motionEvent.action == MotionEvent.ACTION_UP
+            return@setOnTouchListener false
+        }
     }
 
 }
